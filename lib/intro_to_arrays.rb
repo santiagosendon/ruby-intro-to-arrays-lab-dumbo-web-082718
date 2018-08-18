b9b78078be48d8d803ca793276d93c25e6220e86
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-18 18:25:08 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+ my_new_array = []
+  return my_new_array 
+end
+ 
+ def array_with_two_elements
+ my_two_array = Array.new(2)
+  return my_two_array
+end 
+ 
+ def first_element (taylor_swift)
+  my_first_element = taylor_swift [0]
+  return my_first_element
+end
+ 
+ def third_element (taylor_swift)
+  my_third_element = taylor_swift [2]
+  return my_third_element
+end
+
+ def last_element (taylor_swift)
+  my_last_element = taylor_swift [-1]
+  return my_last_element
+end
+ 
+ def first_element_with_array_methods(south_east_asia)
+  first_country = south_east_asia.first 
+  return first_country
+end
+ 
+ def last_element_with_array_methods(south_east_asia)
+  last_country = south_east_asia.last 
+  return last_country
+end
+ 
+ def length_of_array(programming_languages)
+  length = programming_languages.count 
+  return length 
+end
